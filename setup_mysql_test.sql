4f71d89934f1c055627ea946a31bcd814e24f70d
DaisyAram <daisyarama03@gmail.com>
2024-03-19 11:28:46 -0700

    airbnb

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -9,4 +9,3 @@ GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT privilege on performance_schema to hbnb_test
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
